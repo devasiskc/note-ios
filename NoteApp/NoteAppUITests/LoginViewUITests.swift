ee7f3622ba183eaf34018a95cab92dcc415550d7
Devasis KC <devasiskc@gmail.com>
2023-11-30 09:51:59 +0800

    UITEST managed

--- NoteApp/NoteAppUITests/LoginViewUITests.swift
+++ NoteApp/NoteAppUITests/LoginViewUITests.swift
@@ -2,40 +2,53 @@
 //  LoginViewUITests.swift
 //  NoteAppUITests
 //
-//  Created by Neela Rai on 29/11/2023.
+//  Created by Devasis KC on 30/11/2023.
 //
 
 import XCTest
 
-final class LoginViewUITests: XCTestCase {
-
+class LoginViewUITests: XCTestCase {
+    
+    // Provide your app's launch configuration
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // Your app launch code goes here
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testLoginSuccess() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        // Interact with username and password text fields
+        let usernameTextField = app.textFields["Username"]
+        XCTAssertTrue(usernameTextField.exists)
+        usernameTextField.tap()
+        usernameTextField.typeText("testuser")
+        
+        let passwordTextField = app.secureTextFields["Password"]
+        XCTAssertTrue(passwordTextField.exists)
+        passwordTextField.tap()
+        passwordTextField.typeText("testpassword")
+        
+        // Interact with the login button
+        let loginButton = app.buttons["Login"]
+        XCTAssertTrue(loginButton.exists)
+        loginButton.tap()
+        
+        // Verify that the login was successful (adjust based on your actual success criteria)
+        XCTAssertTrue(app.otherElements["Welcome to your Notes"].exists)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testLoginFailure() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        // Interact with the login button without entering credentials
+        let loginButton = app.buttons["Login"]
+        XCTAssertTrue(loginButton.exists)
+        loginButton.tap()
+        
+        // Verify that the error message is displayed (adjust based on your actual error message)
+        XCTAssertTrue(app.staticTexts["Please enter both username and password."].exists)
     }
 }
